8c186dcd017e9ab13c65a59d92afc53b7aa256b6
danielabouelezz <abouelezzdaniel@gmail.com>
2024-07-29 21:44:42 +0300

    adding an example about the usage of the const and the reference in initializer list

---
+++ Class Constructor and Destructor/ConstReferenceInConstructor.cpp
@@ -0,0 +1,50 @@
+/*In a recent code review, senior TL asked you to change the constructor to use const and &.
+    Why do you think so?*/
+
+#include <bits/stdc++.h>
+class A{
+public:
+    A(){
+        std::cout<<"Construct A\n";
+    }
+};
+
+class B{
+public:
+    B(){
+        std::cout<<"Construct B\n";
+    }
+};
+class C{
+public:
+    C(){
+        std::cout<<"Construct C\n";
+    }
+};
+
+class D{
+private:
+    A aa;
+    B bb;
+    C cc;
+public:
+    /* From this:
+    D(A aa, B bb, C cc):
+        aa(aa), bb(bb), cc(cc){}*/
+
+    //To this:
+    D(const A &aa, const B &bb, const C &cc):
+        aa(aa), bb(bb), cc(cc){}
+};
+/*-Using 'const' ensures the original objects are not modified inside the constructor.
+  -Using '&'(references) avoids unecessary copying of objects, which can be more efficient.
+*/
+int main()
+{
+    A a;
+    B b;
+    C c;
+    D d(a, b, c);
+
+    return 0;
+}
